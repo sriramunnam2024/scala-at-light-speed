7031b9c35bca0d3f9e037b0518755c6c691238d3
sriramunnam2024 <sriramunnam2024@gmail.com>
2026-04-14 23:49:54 +0100

    end of excercise 3

--- src/main/scala/com/rockthejvm/Basics.scala
+++ src/main/scala/com/rockthejvm/Basics.scala
@@ -3,49 +3,64 @@ package com.rockthejvm
 object Basics extends App {
 
   // defining a value
-  val meaningOfLife: Int = 42 // val means immutable variable
+  val meaningOfLife: Int = 42 // const int meaningOfLife = 42; // val means immutable variable
 
-  val a_boolean = false
+  // Int, Boolean, Char, Double, Float, String
+  val aBoolean = false // type is optional
 
-  val a_string = "I Love Scala!"
-  val a_composed_string = "I"+" "+"am"+" "+"a"+" "+"love"+"r"+" "+"of"+" "+"Scala"
-  val an_interpolated_string =  s"The meaning of life is $meaningOfLife"
+  // strings and string operations
+  val aString = "I Love Scala!"
+  val aComposedString = "I"+" "+"am"+" "+"a"+" "+"love"+"r"+" "+"of"+" "+"Scala"
+  val anInterpolatedString = s"The meaning of life is $meaningOfLife"
 
-  val an_expression = 2 + 3
+  // expressions = structures that can be reduced to a value
+  val anExpression = 2 + 3
 
-  val if_expression = if (meaningOfLife > 43) 56 else 999
-  val chained_if_expression =
+  // if-expression
+  val ifExpression = if (meaningOfLife > 43) 56 else 999 // in other languages: meaningOfLife > 43 ? 56 : 999
+  val chainedIfExpression =
     if (meaningOfLife > 43) 56
     else if (meaningOfLife < 0) -2
     else if (meaningOfLife > 999) 78
     else 0
 
-
   // code blocks
-  val a_codeblock = {
+  val aCodeBlock = {
     // definitions
-    val a_localvalue = 67
+    val aLocalValue = 67
 
-    // value of block is the value of last expression
-    a_localvalue + 3
+    // value of block is the value of the last expression
+    aLocalValue + 3
   }
+
   // define a function
   def myFunction(x: Int, y: String): String = {
-    y+" "+x
+    y + " " + x
   }
 
   // recursive functions
   def factorial(n: Int): Int =
-    if (n<=1) 1
-    else n*factorial(n-1)
+    if (n <= 1) 1
+    else n * factorial(n - 1)
+
+  /*
+    factorial(5) = 5 * factorial(4) = 5 * 24 = 120
+    factorial(4) = 4 * factorial(3) = 4 * 6
+    factorial(3) = 3 * factorial(2) = 3 * 2
+    factorial(2) = 2 * factorial(1) = 2 * 1
+    factorial(1) = 1
+   */
+
+  // In Scala we don't use loops or iteration, we use RECURSION!
 
   // the unit type = a return type of no meaningful value --> it's the equivalent of "void" in other languages
-  println("I love Scala")
+  // type of SIDE EFFECTS
+  println("I love Scala") // System.out.println, printf, print, console.log
 
   def myUnitReturningFunction(): Unit = {
     println("I don't love returning Unit")
   }
 
-  val theUnit = ()
 
+  val theUnit = ()
 }
